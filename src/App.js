1aa60d86d089f83978e67516f00646a23656f06f
Leyla <157950987+leylamaria@users.noreply.github.com>
2024-04-15 23:32:51 +0200

    Added a footer with links to my portfolio and GitHub repo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,26 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        <p>
+          This project was coded by{" "}
+          <a
+            href="https://yinarts.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Leyla Maria{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/leylamaria/yinarts-weather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
