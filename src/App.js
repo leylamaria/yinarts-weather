193ca79e38af606b85e4232c02a7a3a2214136da
Leyla <157950987+leylamaria@users.noreply.github.com>
2024-04-16 09:37:44 +0200

    Installed and integrated Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,27 +3,29 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <p>
-          This project was coded by{" "}
-          <a
-            href="https://yinarts.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Leyla Maria{" "}
-          </a>
-          and is{" "}
-          <a
-            href="https://github.com/leylamaria/yinarts-weather"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on GitHub
-          </a>
-        </p>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <p>
+            This project was coded by{" "}
+            <a
+              href="https://yinarts.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Leyla Maria{" "}
+            </a>
+            and is{" "}
+            <a
+              href="https://github.com/leylamaria/yinarts-weather"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
